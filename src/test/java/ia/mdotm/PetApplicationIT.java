eb290ea1c5c3bae959ce060976a3ac551710887e
Gundar <gundar.work@gmail.com>
2026-02-05 00:56:06 +0100

    Added test to verify the context loads

---
+++ src/test/java/ia/mdotm/PetApplicationIT.java
@@ -0,0 +1,15 @@
+package ia.mdotm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PetApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
